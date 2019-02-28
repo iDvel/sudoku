3d078814e1ebf7957149fce5c099cd317e549827
Dvel <git@dvel.me>
2019-02-28 15:32:12 +0800

    Add 扫看方法

--- sudoku.py
+++ sudoku.py
@@ -9,6 +9,10 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Sudoku:
+    def __init__(self):
+        # 计数
+        self.saokan_count = 0
+
     def start(self):
         # 数独二维数组
         self.l = self.init_suduku_list()
@@ -16,8 +20,9 @@ class Sudoku:
         self.initial_list = copy.deepcopy(self.l)
 
         # start!
-        self.show()
+        self.show(key='num')
         self.fill_candidates()
+        self.fill_number()
 
     def init_suduku_list(self):
         """创建初始的数独组合：二维数组"""
@@ -66,11 +71,19 @@ class Sudoku:
                                  'col': j}
         return input_l
 
-    def get_list_of(self, position, number):
-        """拿取整个宫、行、列的列表"""
-        return [unit for row_list in self.l for unit in row_list if unit[position] == number]
+    def get_list_of(self, position, number=None):
+        """拿取宫、行、列的列表"""
+        if position == 'boxes':
+            # 返回包含 9 个宫的二维数组，每一组是一宫
+            boxes = []
+            for i in range(9):
+                boxes.append([unit for row_list in self.l for unit in row_list if unit['box'] == i])
+            return boxes
+        else:
+            # 返回单一宫、行、列的列表
+            return [unit for row_list in self.l for unit in row_list if unit[position] == number]
 
-    def show(self, key='num'):
+    def show(self, key='can'):
         """图形化显示数独二维数组"""
         if key == 'num':
             base_graph = '''
@@ -86,8 +99,7 @@ class Sudoku:
             | {0[6][0][num]} {0[6][1][num]} {0[6][2][num]} | {0[6][3][num]} {0[6][4][num]} {0[6][5][num]} | {0[6][6][num]} {0[6][7][num]} {0[6][8][num]} |
             | {0[7][0][num]} {0[7][1][num]} {0[7][2][num]} | {0[7][3][num]} {0[7][4][num]} {0[7][5][num]} | {0[7][6][num]} {0[7][7][num]} {0[7][8][num]} |
             | {0[8][0][num]} {0[8][1][num]} {0[8][2][num]} | {0[8][3][num]} {0[8][4][num]} {0[8][5][num]} | {0[8][6][num]} {0[8][7][num]} {0[8][8][num]} |
-            +-------+-------+-------+
-            '''
+            +-------+-------+-------+'''
         elif key == 'can':
             base_graph = '''
             +----------------------------+----------------------------+----------------------------+
@@ -102,8 +114,7 @@ class Sudoku:
             | {0[6][0][can]:^9}{0[6][1][can]:^9}{0[6][2][can]:^9}| {0[6][3][can]:^9}{0[6][4][can]:^9}{0[6][5][can]:^9}| {0[6][6][can]:^9}{0[6][7][can]:^9}{0[6][8][can]:^9}|
             | {0[7][0][can]:^9}{0[7][1][can]:^9}{0[7][2][can]:^9}| {0[7][3][can]:^9}{0[7][4][can]:^9}{0[7][5][can]:^9}| {0[7][6][can]:^9}{0[7][7][can]:^9}{0[7][8][can]:^9}|
             | {0[8][0][can]:^9}{0[8][1][can]:^9}{0[8][2][can]:^9}| {0[8][3][can]:^9}{0[8][4][can]:^9}{0[8][5][can]:^9}| {0[8][6][can]:^9}{0[8][7][can]:^9}{0[8][8][can]:^9}|
-            +----------------------------+----------------------------+----------------------------+
-            '''
+            +----------------------------+----------------------------+----------------------------+'''
         base_graph = base_graph.format(self.l)
         print(base_graph)
 
@@ -121,7 +132,35 @@ class Sudoku:
                     s = {'1', '2', '3', '4', '5', '6', '7', '8', '9'} - s
                     unit['can'] = ''.join(sorted(list(s)))
         print('{:^100}'.format('*** 潜在数字填写完毕 ***'), end='')
-        self.show(key='can')
+        self.show()
+
+    def fill_number(self):
+        """计算数字并填充"""
+        compare_list = copy.deepcopy(self.l)
+        while True:
+            self.method_saokan()
+            if (compare_list == self.l):
+                print('{:^100}'.format('*** 扫看循环结束 ***'), end='')
+                break
+            else:
+                compare_list = copy.deepcopy(self.l)
+
+    def method_saokan(self):
+        """扫看：候选数字在当前宫唯一，则此格为此数"""
+        for box in self.get_list_of('boxes'):
+            for unit in box:
+                if len(unit['can']) == 1:
+                    the_one = unit['can']
+                    # 填入数字，删除同行同列的相同候选数字
+                    unit['num'] = unit['can']
+                    unit['can'] = '[{}]'.format(unit['can'])
+                    for u in self.get_list_of('row', unit['row']):
+                        u['can'] = u['can'].strip(the_one)
+                    for u in self.get_list_of('col', unit['col']):
+                        u['can'] = u['can'].strip(the_one)
+        print('{:^100}'.format('*** 第 {} 次扫看完毕 ***'.format(self.saokan_count)), end='')
+        self.saokan_count += 1
+        self.show()
 
 
 sudoku = Sudoku()
